4dfa41b77e06b61950e80d289666fca4fb9c7ccf
Cesar Gomez <cgomez29333@gmail.comgit config --global user.email cgomez29333@gmail.com>
2024-06-21 11:08:02 -0500

    se agrego  alarma sonora

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,7 +1,10 @@
 import hamburgerMenu from "./menu-hamburguesa.js";
+import { digitalClock, alarm } from "./reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
+  alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
 });
