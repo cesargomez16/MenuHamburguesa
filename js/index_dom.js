9a7a8eaa6d945a3993d62bf207ecd702dc376f37
Cesar Gomez <cgomez29333@gmail.comgit config --global user.email cgomez29333@gmail.com>
2024-09-25 11:13:15 -0500

    se agregaron ejercicios de js

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,10 +1,23 @@
 import hamburgerMenu from "./menu-hamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
+import { moveBall, shortcuts } from "./teclado.js";
+import countdown from "./cuenta_regresiva.js";
+/* import scrollTopButton from "./boton.js"; */
+import darkTheme from "./tema_oscuro.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
-  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown("countdown", " dec 31 2024 23:59:59", "feliz cumpleaños 🤓 🤓");
+  /* scrollTopButton("scroll-top-btn"); */
 });
+
+d.addEventListener("keydown", (e) => {
+  shortcuts(e);
+  moveBall(e, ".ball", ".stage");
+});
+
+darkTheme(".dark-theme-btn", "dark-mode");
